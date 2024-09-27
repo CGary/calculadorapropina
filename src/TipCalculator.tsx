904b909bd7dfc6cbd6c3e4b9ab6a88100f509ff1
Cristian Gary Nuñez Jimenez <cristiangnj@gmail.com>
2024-09-27 10:49:13 -0400

    feat: historial - initial commit

--- src/TipCalculator.tsx
+++ src/TipCalculator.tsx
@@ -1,20 +1,49 @@
 import React, { useState } from "react";
+import { Hystory } from './history'
 
 const TipCalculator: React.FC = () => {
   const [billAmount, setBillAmount] = useState<number>(0);
   const [tipPercentage, setTipPercentage] = useState<number>(0);
   const [tipAmount, setTipAmount] = useState<number>(0);
   const [totalAmount, setTotalAmount] = useState<number>(0);
+  const [history,setHistory]=useState<{
+    bill: number,
+    tipPercentage: number,
+    tip: number,
+    total: number
+  }[]>([])
+
 
   const calculateTip = () => {
     const tip = (billAmount * tipPercentage) / 100;
     setTipAmount(tip);
     setTotalAmount(billAmount + tip);
+    setHistory((historyPrev)=>{
+      const newHistory={
+        bill: billAmount,
+        tipPercentage: tipPercentage,
+        tip: tip,
+        total: totalAmount 
+      }
+
+      return newHistory
+    })
+    
   };
+
+  const deleteAllHistory = () => {
+    const confirmation=window.confirm("Estas seguro que quieres eliminar el hisotrial?")
+    if(confirmation){
+      setHistory([]) 
+    }
+  }
 
   return (
     <div>
       <h1>Calculadora de Propinas</h1>
+      <div>
+        <button onClick={deleteAllHistory}>Borrar todo el historial</button>
+      </div>
       <div>
         <label>
           Monto de la factura:
@@ -40,6 +69,8 @@ const TipCalculator: React.FC = () => {
         <h2>Monto de la Propina: ${tipAmount.toFixed(2)}</h2>
         <h2>Total a Pagar: ${totalAmount.toFixed(2)}</h2>
       </div>
+
+      <History history={history} />
     </div>
   );
 };
---
+++ src/history.tsx
@@ -0,0 +1,15 @@
+export interface HistoryType {
+	bill: number,
+    tipPercentage:number,
+    tip:number,
+    total: number
+}
+
+export const Hystory = (history: HistoryType[]) => {
+
+	return (
+		
+	)
+}
+
+
